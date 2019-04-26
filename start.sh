8adc67fda869f25db7afb563b16b120ff754fc0a
Michael Plainer <plainer@ymail.com>
2019-04-26 18:55:09 +0200

    Initial commit
    Add a basic dockerfile starting Fedora

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qemu-system-riscv64 -nographic -machine virt -smp 4 -m 2G -kernel bbl -object rng-random,filename=/dev/urandom,id=rng0 -device virtio-rng-device,rng=rng0 -append "console=ttyS0 ro root=/dev/vda" -device virtio-blk-device,drive=hd0 -drive file=stage4-disk.img,format=raw,id=hd0 -device virtio-net-device,netdev=usernet -netdev user,id=usernet,hostfwd=tcp::10000-:22
